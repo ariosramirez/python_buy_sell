862c110970a77e628efc4dbb69ff7f4a41217f30
Andres David Rios Ramirez <andres.riosramirez@mercadolibre.com>
2019-10-29 09:42:44 -0300

    added cli.py and main (python_buy_sell)

--- python_buy_sell/cli.py
+++ python_buy_sell/cli.py
@@ -3,16 +3,22 @@
 """Console script for python_buy_sell."""
 import sys
 import click
+from .clients import commands as clients_commands
 
 
-@click.command()
-def main(args=None):
+@click.group()
+@click.pass_context
+def main(ctx, args=None):
     """Console script for python_buy_sell."""
     click.echo("Replace this message by putting your code into "
                "python_buy_sell.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+    ctx.obj = {}
+
+#    return 0
+
+main.add_command(clients_commands.all)
 
 
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+#if __name__ == "__main__":
+#    sys.exit(main())  # pragma: no cover
